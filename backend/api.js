7d496858790e156b2fd132518b8dfce3d2f0e4ad
Tuan Cai <tcai44@gatech.edum>
2024-01-17 20:52:08 -0500

    feat: db Connection

--- backend/api.js
+++ backend/api.js
@@ -1,28 +1,29 @@
-import express from 'express';
-import { database } from './database.js';
+import express from "express";
+import { database } from "./database.js";
 import cors from "cors";
+import userRoutes from "./routes/userRoutes.js"; // Adjust the path as needed
+import connectDB from "./config/dbConn.js";
+import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+dotenv.config();
+
+// Connect to the database
+connectDB();
 const app = express();
-const port = 5000;
+const port = 5001;
+
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-app.get('/api/bog/users', (req, res) => {
-    res.json(database).status(200);
-});
-
-
-app.get('/api/bog/users/:id', (req, res) => {
-    const user = database.filter((user) => user.id === req.params.id)[0]
-    res.json(user).status(200)
-});
-
+app.use("/api/bog/users", userRoutes);
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(port, () => console.log(`Server running on port ${port}`));
 });
---
+++ backend/config/dbConn.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    console.log(process.env);
+    await mongoose.connect(process.env.DATABASE_URI);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default connectDB;
